f0747cee2a9292fc8fa3d23b8617d3b085d83a16
danielpqb <danielpqb@gmail.com>
2022-10-18 21:24:06 -0300

    usersRoute

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 import express from "express";
 import cors from "cors";
 
-import usersRouter from "./routers/users.router.js";
+import usersRoute from "./routes/users.route.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.use(usersRouter);
+app.use(usersRoute);
 
 export default app;
