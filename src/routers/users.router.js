820802c50a97e334c57a942ca0b4194955d6da6d
danielpqb <danielpqb@gmail.com>
2022-10-20 20:42:47 -0300

    Get User data using token

--- src/routers/users.router.js
+++ src/routers/users.router.js
@@ -3,7 +3,8 @@ import express from "express";
 import {
   postSignUpUser,
   postSignInUser,
-  getUsersWithFilter
+  getUserDataByToken,
+  getUsersWithFilter,
 } from "../controllers/users.controller.js";
 
 import { validateSchema } from "../middlewares/validateSchema.middleware.js";
@@ -13,17 +14,11 @@ import * as schemas from "../schemas/users.schemas.js";
 
 const router = express.Router();
 
-router.post(
-  "/signup",
-  validateSchema(schemas.postSignUpUser),
-  postSignUpUser
-);
+router.post("/signup", validateSchema(schemas.postSignUpUser), postSignUpUser);
 
-router.post(
-  "/signin",
-  validateSchema(schemas.postSignInUser),
-  postSignInUser
-);
+router.post("/signin", validateSchema(schemas.postSignInUser), postSignInUser);
+
+router.get("/users/me", validateToken, getUserDataByToken);
 
 router.get(
   "/searchUsers",
