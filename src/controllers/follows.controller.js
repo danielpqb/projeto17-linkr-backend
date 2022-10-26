6d1e09a390882a218503bea8036e1f8f3616267b
g-rmc <g.rmc3000@gmail.com>
2022-10-26 16:08:37 -0300

    Created follows querys and functions

--- src/controllers/follows.controller.js
+++ src/controllers/follows.controller.js
@@ -1,8 +1,31 @@
+import * as followRepositories from "../repositories/follows.repository.js";
 
 export async function createFollow(req, res) {
-    res.send("createFollow");
+    const { follower, followed } = res.locals;
+    try {
+        const alreadyFollow = await followRepositories.getFollow(follower,followed);
+        if (alreadyFollow.rowCount !== 0){
+            res.status(400).send({message: "you already follow this user"});
+            return;
+        }
+        await followRepositories.addFollow(follower, followed);
+        res.sendStatus(201);
+    } catch (error) {
+        res.sendStatus(500);
+    }
 }
 
 export async function deleteFollow(req, res) {
-    res.send("deleteFollow");
+    const { follower, followed } = res.locals;
+    try {
+        const notFollow = await followRepositories.getFollow(follower,followed);
+        if (notFollow.rowCount === 0){
+            res.status(400).send({message: "you don't follow this user"});
+            return;
+        }
+        await followRepositories.deleteFollow(follower, followed);
+        res.sendStatus(204);
+    } catch (error) {
+        res.sendStatus(500);
+    }
 }
--- src/routers/follows.router.js
+++ src/routers/follows.router.js
@@ -2,10 +2,11 @@ import { Router } from "express";
 
 import { validateToken } from "../middlewares/validateToken.middleware.js";
 import { createFollow, deleteFollow } from "../controllers/follows.controller.js";
+import { validateIds } from "../middlewares/follows.middleware.js";
 
 const router = Router();
 
-router.post("/follow/:id", validateToken, createFollow);
-router.delete("/follow/:id", validateToken, deleteFollow);
+router.post("/follow/:id", validateToken, validateIds, createFollow);
+router.delete("/follow/:id", validateToken, validateIds, deleteFollow);
 
 export default router;
